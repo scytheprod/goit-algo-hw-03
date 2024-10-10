8230756e51fcd2c3e19b3a50e4e16c12d7e058ae
scytheprod <vkrivtsov832@gmail.com>
2024-10-10 15:16:39 +0300

    goit-algo-hw-03

--- HomeWork1.py
+++ HomeWork1.py
@@ -5,11 +5,11 @@ def get_days_from_today(date):
     try:
         time_now = datetime.today()
         time_now1 = time_now.date()
-        print(time_now1)
+        #print(time_now1)
         date_object = datetime.strptime(date, "%Y-%m-%d").date()
-        print(date_object)
+        #print(date_object)
         diff_time = time_now1 - date_object
-        print(diff_time.days)
+        #print(diff_time.days)
         return diff_time.days
     except ValueError:
         print("Not right value! Try 'YYYY-MM-DD'")
