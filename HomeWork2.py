9e1dd9297ceda3075d235a64b51abd88c646fa0f
scytheprod <vkrivtsov832@gmail.com>
2024-10-10 15:51:26 +0300

    goit-algo-hw-03

---
+++ HomeWork2.py
@@ -0,0 +1,16 @@
+import random
+
+
+def get_numbers_ticket(min, max, quantity):
+    try:
+        numbers = list(range(min, max))
+        if max <= 1000 and min >=1:
+            random_numbers = random.sample(numbers, quantity)
+
+            return sorted(random_numbers)
+        else:
+            return list()
+    except ValueError:
+        return list()
+    except TypeError:
+        return list()
